0b1b082c5912ae2ea21f6ebcf895fbf8c5577cb8
DJBen <lsh32768@gmail.com>
2014-11-24 18:34:04 -0500

    removed submodule ImageScout

---
+++ FNAsyncGallery/GalleryExampleViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GalleryExampleViewController.swift
+//  FNAsyncGallery
+//
+//  Created by Sihao Lu on 11/20/14.
+//  Copyright (c) 2014 DJ.Ben. All rights reserved.
+//
+
+import UIKit
+
+class GalleryExampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
