bb7a500fb9e3ea55405a3da01e6c8256a77f99d4
DJBen <lsh32768@gmail.com>
2014-12-21 18:16:20 +0800

    Gallery now supports photo browsing

---
+++ FNAsyncGallery/Classes/GalleryViewControllerTransitioning.swift
@@ -0,0 +1,13 @@
+//
+//  GalleryViewControllerTransitioning.swift
+//  FNAsyncGallery
+//
+//  Created by Sihao Lu on 12/21/14.
+//  Copyright (c) 2014 DJ.Ben. All rights reserved.
+//
+
+import UIKit
+
+class GalleryViewControllerTransitioning: NSObject {
+   
+}
